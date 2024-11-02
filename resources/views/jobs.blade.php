5c96bc95cd5830592fb56ac1179ff8527db91422
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-11-02 14:59:44 +0530

    database seeders

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,7 +4,7 @@
     </x-slot:heading>
     <div class="space-y-5">
         @foreach ($jobs as $job)
-   <a href="/jobs/{{ $job['id'] }}">
+   <a href="/jobs/{{ $job['id'] }}" class="block">
     <div class="bg-white p-5 rounded-lg border-gray-500">
 <div class="text-blue-500"> {{ $job->employer->name }}</div>
 <p>
@@ -14,7 +14,7 @@
 </a>    
         @endforeach
 
-        <div>
+        <div class="pt-5 pb-10">
             {{ $jobs->links() }}
         </div>
     </div>
